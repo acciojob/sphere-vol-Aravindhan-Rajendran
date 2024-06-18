82bdef45f8d997013d31404c06b8118c0ae38feb
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-18 19:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function volume_sphere() {
-    //Write your code here
-  
+const pi=3.141
+function volume_sphere(r) {
+    if(r>=0 && !isNaN(r))
+    {
+        var volume=(4/3)*pi*r*r*r;
+        return volume.toFixed(4);
+    }
+    else
+    {
+        return 'NaN';
+    }
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = function(event) {
+        event.preventDefault();
+        var r = document.getElementById('radius').value;
+        document.getElementById('volume').value = volume_sphere(r);
+    };
+};
+
